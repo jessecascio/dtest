428f7b897524f4c6999c576a58880c78bddf989d
Jesse Cascio <jesse.cascio@timeinc.com>
2018-08-27 18:42:50 -0700

    comment update

--- src/algorithm/graph-breadth-search/template.js
+++ src/algorithm/graph-breadth-search/template.js
@@ -4,15 +4,8 @@
 
 module.exports = {
   /**
-   * Determine if two vertices are connected
-   * @param Object (graph representation) - { a:[['a',2],['c',3]], b:[['a',3]], c:[[]]}
-   * @return bool
-   */
-  connected: null,
-
-  /**
    * Return the vertices in the search path if they are connected
-   * @param Object (graph representation) - { a:[['a',2],['c',3]], b:[['a',3]], c:[[]]}
+   * @param Object (graph representation) - { a:['c','b'], b:[['a']], c:[['a]]}
    * @return Array<T>
    */
   search: null
